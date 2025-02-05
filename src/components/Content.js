1aec01bae3c8104fb0f858daa0663b7975755e7b
piepie22 <pieczynskimandy@yahoo.com>
2025-02-04 19:23:21 -0500

    static

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => (
+  <div className="content">
+    <p>Once upon a time, there was a young girl whom very dearly wanted to go to the ball but her evil stepmother disapproved.</p>
+    <p>Overwhelmed with emotions Cinderella ran off and encountered her Fairy Godmother, she was given a dress, glass slippers and a carriage to attend. However she only had until midnight.</p>
+    <p>Cinderella attended the ball and fell in love with the Prince. As the clock struck midnight she ran back home leaving behind a glass slipper. The prince searched throughout the kingdom for the perfect fit for that shoe and found Cinderella.</p>
+  </div>
+);
+
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <p>Moral of the story- Try new things.</p>
+  </footer>
+);
+
+export default Footer;
